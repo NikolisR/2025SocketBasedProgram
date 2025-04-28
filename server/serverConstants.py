a0df40086123d365dbaa5f41e74658a524e817e9
niko <nlr11@pct.edu>
2025-04-28 17:56:41 -0400

    Added back .idea and requirements.txt

--- server/serverConstants.py
+++ server/serverConstants.py
@@ -1,5 +1,5 @@
 # change IP Address to computer IP for LAN or IP for port forwarding
-MYIPADDRESS = '0.0.0.0'
+MYIPADDRESS = '0.0.0.0'                         # 10.7.87.77: My current laptop IP
 PORT = 5555
 
 TOTALROUNDS = 5
